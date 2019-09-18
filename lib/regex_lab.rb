55a6f4832dd54acea9e0ac3ceeae46b4d09c9336
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-18 18:04:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,8 +4,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  binding.pry
-  text.scan(//)
+  #binding.pry
+  text.scan(/\bun[a-z]+ing\b/)
 end
 
 def words_five_letters_long(text)
